8e0c1a2ae80131b92ac8ca60ecb195dad7c6af2a
Lahoudere Fabien <fabien.lahoudere@openwide.fr>
2014-04-28 11:53:44 +0200

    add a test tool to dump network packet on an interface

--- test/dump.c
+++ test/dump.c
@@ -1,27 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <linux/if_ether.h>
 #include <net/ethernet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <poll.h>
+#include <netdb.h>
 
 #include "packet.h"
 #include "ethernet.h"
 #include "arp.h"
 #include "ip.h"
 
+static void show_arp( char * buffer, int len )
+{
+    if ( !buffer || len < 0 )
+        return;
+    printf("ARP\n");
+}
+
+static void show_ip( char * buffer, int len )
+{
+    struct in_addr tmp;
+    struct iphdr * hdr;
+    struct protoent * proto;
+
+    if ( len < 20 )
+        return;
+
+    hdr = (struct iphdr *) buffer;
+
+    tmp.s_addr = hdr->daddr;
+    printf("IP dest: %s\n", inet_ntoa(tmp) );
+
+    tmp.s_addr = hdr->saddr;
+    printf("IP src: %s\n", inet_ntoa(tmp) );
+
+    proto = getprotobynumber(hdr->protocol);
+    printf("IP protocol: %s\n", proto->p_name );
+}
+
 static void analyze_packet( char * buffer, int len )
 {
+    struct ether_header * hdr;
+
     if ( !buffer )
         printf("No buffer to treat\n");
 
-    printf("Mac dest: %02x:%02x:%02x:%02x:%02x:%02x\n",
-           buffer[0]&0xf,buffer[1]&0xf,buffer[2]&0xf,buffer[3]&0xf,buffer[4]&0xf,buffer[5]&0xf);
-    printf("Mac dest: %02x:%02x:%02x:%02x:%02x:%02x\n",
-           buffer[6]&0xf,buffer[7]&0xf,buffer[8]&0xf,buffer[9]&0xf,buffer[10]&0xf,buffer[11]&0xf);
-    printf("Mac type: %02x%02x\n", buffer[12],buffer[13]);
+    hdr = (struct ether_header *)buffer;
+
+    if ( len >= ETH_HLEN )
+    {
+        printf("Mac dest: %02x:%02x:%02x:%02x:%02x:%02x\n",
+               buffer[0]&0xff,buffer[1]&0xff,buffer[2]&0xff,buffer[3]&0xff,buffer[4]&0xff,buffer[5]&0xff);
+        printf("Mac dest: %02x:%02x:%02x:%02x:%02x:%02x\n",
+               buffer[6]&0xff,buffer[7]&0xff,buffer[8]&0xff,buffer[9]&0xff,buffer[10]&0xff,buffer[11]&0xff);
+        printf("Mac type: %04x\n", ntohs(hdr->ether_type) );
+        switch( ntohs(hdr->ether_type) )
+        {
+        case ETH_P_IP:
+            show_ip( buffer + ETH_HLEN, len - ETH_HLEN );
+            break;
+        case ETH_P_ARP:
+            show_arp( buffer + ETH_HLEN, len - ETH_HLEN );
+            break;
+        default:
+            printf("Unknown type: %d\n",  ((unsigned short*)buffer)[6] );
+            break;
+        }
+        printf("\n");
+    }
 }
 
 int main ( int argc, char **argv )
